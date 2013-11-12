4677ef764854a60dc0652487699c3b56878efec8
DienNT <diennt@smartosc.com>
2013-11-12 23:42:31 +0700

    generate core bundle

---
+++ src/Tarazz/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tarazz\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TarazzCoreBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Tarazz/CoreBundle/TarazzCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tarazz\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TarazzCoreBundle extends Bundle
+{
+}
