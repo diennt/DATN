9d5ff1594c9501a3107e0f0243c878a6a4d4e8a6
DienNT <diennt@smartosc.com>
2013-11-20 22:33:17 +0700

    implement mopa bootstrap

---
+++ src/Tarazz/CoreBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tarazz\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('TarazzCoreBundle:Default:index.html.twig', array());
+    }
+}
