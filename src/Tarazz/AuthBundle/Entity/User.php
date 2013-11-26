d2765a0cc29543e2f47013f99ead100dd49b826c
DienNT <diennt@smartosc.com>
2013-11-26 17:49:45 +0700

    complete login style

--- src/Tarazz/AuthBundle/Entity/User.php
+++ src/Tarazz/AuthBundle/Entity/User.php
@@ -15,65 +15,9 @@ class User extends BaseUser
      */
     protected  $id;
 
-    /**
-     * @var integer
-     */
-    private $groupId;
-
-    /**
-     * @var integer
-     */
-    private $timeZoneId;
-
     public function __construct()
     {
         parent::__construct();
         // your own logic
-    }
-
-    /**
-     * Set groupId
-     *
-     * @param integer $groupId
-     * @return User
-     */
-    public function setGroupId($groupId)
-    {
-        $this->groupId = $groupId;
-    
-        return $this;
-    }
-
-    /**
-     * Get groupId
-     *
-     * @return integer 
-     */
-    public function getGroupId()
-    {
-        return $this->groupId;
-    }
-
-    /**
-     * Set timeZoneId
-     *
-     * @param integer $timeZoneId
-     * @return User
-     */
-    public function setTimeZoneId($timeZoneId)
-    {
-        $this->timeZoneId = $timeZoneId;
-    
-        return $this;
-    }
-
-    /**
-     * Get timeZoneId
-     *
-     * @return integer 
-     */
-    public function getTimeZoneId()
-    {
-        return $this->timeZoneId;
     }
 }
